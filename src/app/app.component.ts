23e4cea29c266051a47bc581c09ae417a44ae63f
v1cke <v1cke1982@gmail.com>
2022-09-04 20:11:45 +0200

    added images and start slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gruenwald.de';
+  images = ['gaming_then.jpg','gaming_now.jpg','gaming_future.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+
+  updateImage(){
+    setInterval(()=> {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 5);
+    }, 5000);
+  }
+
+
+
+
+
+
+
 }
